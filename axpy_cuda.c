7afdf16986e6a595368e0d4f32a9cadc6270f9ad
Xinyao Yi <yxyict@gmail.com>
2020-08-03 10:03:31 +0000

    Added the code for AXPY Three versions are added. 1 element/thread, block distribution, cyclic distribution

---
+++ axpy_cuda.c
@@ -0,0 +1,96 @@
+// Experimental test input for Accelerator directives
+//  simplest scalar*vector operations
+// Liao 1/15/2013
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <sys/timeb.h>
+#include "axpy.h"
+
+double read_timer_ms() {
+    struct timeb tm;
+    ftime(&tm);
+    return (double) tm.time * 1000.0 + (double) tm.millitm;
+}
+
+/* change this to do saxpy or daxpy : single precision or double precision*/
+#define REAL double
+#define VEC_LEN 1024000 //use a fixed number for now
+/* zero out the entire vector */
+void zero(REAL *A, int n)
+{
+    int i;
+    for (i = 0; i < n; i++) {
+        A[i] = 0.0;
+    }
+}
+
+/* initialize a vector with random floating point numbers */
+void init(REAL *A, int n)
+{
+    int i;
+    for (i = 0; i < n; i++) {
+        A[i] = (double)drand48();
+    }
+}
+
+/*serial version */
+void axpy(REAL* x, REAL* y, long n, REAL a) {
+  int i;
+  for (i = 0; i < n; ++i)
+  {
+    y[i] += a * x[i];
+  }
+}
+
+/* compare two arrays and return percentage of difference */
+REAL check(REAL*A, REAL*B, int n)
+{
+    int i;
+    REAL diffsum =0.0, sum = 0.0;
+    for (i = 0; i < n; i++) {
+        diffsum += fabs(A[i] - B[i]);
+        sum += fabs(B[i]);
+    }
+    return diffsum/sum;
+}
+
+int main(int argc, char *argv[])
+{
+  int n;
+  REAL *y_cuda, *y, *x;
+  REAL a = 123.456;
+
+  n = VEC_LEN;
+  fprintf(stderr, "Usage: axpy <n>\n");
+  if (argc >= 2) {
+    n = atoi(argv[1]);
+  }
+  y_cuda = (REAL *) malloc(n * sizeof(REAL));
+  y  = (REAL *) malloc(n * sizeof(REAL));
+  x = (REAL *) malloc(n * sizeof(REAL));
+
+  srand48(1<<12);
+  init(x, n);
+  init(y_cuda, n);
+  memcpy(y, y_cuda, n*sizeof(REAL));
+
+  axpy(x, y, n, a);
+
+  int i;
+  int num_runs = 10;
+  /* cuda version */
+  double elapsed = read_timer_ms();
+  for (i=0; i<num_runs; i++) axpy_cuda(x, y, n, a);
+  elapsed = (read_timer_ms() - elapsed)/num_runs;
+
+  REAL checkresult = check(y_cuda, y, n);
+  printf("axpy(%d): checksum: %g, time: %0.2fms\n", n, checkresult, elapsed);
+  //assert (checkresult < 1.0e-10);
+
+  free(y_cuda);
+  free(y);
+  free(x);
+  return 0;
+}
---
+++ axpy_cudakernel.cu
@@ -0,0 +1,55 @@
+#include "axpy.h"
+
+__global__ 
+void
+axpy_cudakernel_1perThread(REAL* x, REAL* y, int n, REAL a)
+{
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < n) y[i] += a*x[i];
+}
+
+/* block distribution of loop iteration */
+__global__ 
+void axpy_cudakernel_block(REAL* x, REAL* y, int n, REAL a) {
+	int thread_num = threadIdx.x + blockIdx.x * blockDim.x;
+	int total_threads = gridDim.x * blockDim.x;
+
+	int block_size = n / total_threads; //dividable, TODO handle non-dividiable later
+	
+	int start_index = thread_num * block_size;
+	int stop_index = start_index + block_size;
+	int i;
+        for (i=start_index; i<stop_index; i++) {
+		if (i < n) y[i] += a*x[i];
+	}
+}
+
+/* cyclic distribution of loop distribution */
+__global__
+void axpy_cudakernel_cyclic(REAL* x, REAL* y, int n, REAL a) {
+	int thread_num = threadIdx.x + blockIdx.x * blockDim.x;
+	int total_threads = gridDim.x * blockDim.x;
+	
+	int i;
+	for (i=thread_num; i<n; i+=total_threads) { 
+		if (i < n) y[i] += a*x[i];
+	}
+}
+
+void axpy_cuda(REAL* x, REAL* y, int n, REAL a) {
+  REAL *d_x, *d_y;
+  cudaMalloc(&d_x, n*sizeof(REAL));
+  cudaMalloc(&d_y, n*sizeof(REAL));
+
+  cudaMemcpy(d_x, x, n*sizeof(REAL), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_y, y, n*sizeof(REAL), cudaMemcpyHostToDevice);
+
+  // Perform axpy elements
+  axpy_cudakernel_1perThread<<<(n+255)/256, 256>>>(d_x, d_y, n, a);
+  axpy_cudakernel_block<<<1024, 256>>>(d_x, d_y, n, a);
+  axpy_cudakernel_cyclic<<<1024, 256>>>(d_x, d_y, n, a);
+
+  cudaMemcpy(y, d_y, n*sizeof(REAL), cudaMemcpyDeviceToHost);
+  cudaFree(d_x);
+  cudaFree(d_y);
+}
