fb4e4f1be10166113dd50e51eaaa2fc1844b0477
Xinyao Yi <yxyict@gmail.com>
2020-08-30 03:26:32 +0000

    Added the unified memory version for the "different part" case of memoryThrashing

---
+++ memoryThrashing/differentPart_unified/axpy_cuda.cu
@@ -0,0 +1,172 @@
+// Experimental test input for Accelerator directives
+//  simplest scalar*vector operations
+// Liao 1/15/2013
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <sys/timeb.h>
+#include "axpy.h"
+
+double read_timer_ms() {
+    struct timeb tm;
+    ftime(&tm);
+    return (double) tm.time * 1000.0 + (double) tm.millitm;
+}
+
+/* change this to do saxpy or daxpy : single precision or double precision*/
+#define REAL double
+#define VEC_LEN 1024000 //use a fixed number for now
+/* zero out the entire vector */
+void zero(REAL *A, int n)
+{
+    int i;
+    for (i = 0; i < n; i++) {
+        A[i] = 0.0;
+    }
+}
+
+/* initialize a vector with random floating point numbers */
+void init(REAL *A, int n)
+{
+    int i;
+    for (i = 0; i < n; i++) {
+        A[i] = (double)drand48();
+    }
+}
+
+/*serial version */
+void axpy(REAL* x, REAL* y, long n, REAL a) {
+  int i;
+  for (i = 0; i < n; ++i)
+  {
+    x[i] = a*x[i];
+    y[i] += x[i];
+  }
+}
+
+/* compare two arrays and return percentage of difference */
+REAL check(REAL*A, REAL*B, int n)
+{
+    int i;
+    REAL diffsum =0.0, sum = 0.0;
+    for (i = 0; i < n; i++) {
+        diffsum += fabs(A[i] - B[i]);
+        sum += fabs(B[i]);
+    }
+    return diffsum;
+}
+
+int main(int argc, char *argv[])
+{
+  int n;
+  REAL *x, *x_cuda, *x_cuda1, *y, *y_cuda, *y_cuda1;
+  REAL a = 123.456;
+
+  n = VEC_LEN;
+  fprintf(stderr, "Usage: axpy <n>\n");
+  if (argc >= 2) {
+    n = atoi(argv[1]);
+  }
+
+  x = (REAL *) malloc(n * sizeof(REAL));
+  x_cuda = (REAL *) malloc(n * sizeof(REAL));
+  x_cuda1 = (REAL *) malloc(n * sizeof(REAL));
+  
+  y  = (REAL *) malloc(n * sizeof(REAL));
+  y_cuda = (REAL *) malloc(n * sizeof(REAL));
+  y_cuda1 = (REAL *) malloc(n * sizeof(REAL));
+ 
+
+  srand48(1<<12);
+  init(x, n);
+  init(y, n);
+  memcpy(y_cuda, y, n*sizeof(REAL));
+  memcpy(x_cuda, x, n*sizeof(REAL));
+  memcpy(y_cuda1, y, n*sizeof(REAL));
+  memcpy(x_cuda1, x, n*sizeof(REAL));
+
+  double elapsed2 = read_timer_ms();
+  REAL *x2, *y2;
+  cudaMallocManaged(&x2, n*sizeof(REAL));
+  cudaMallocManaged(&y2, n*sizeof(REAL));
+  elapsed2 = (read_timer_ms() - elapsed2);
+
+  memcpy(y2, y, n*sizeof(REAL));
+  memcpy(x2, x, n*sizeof(REAL));
+
+
+
+  int i;
+  int num_runs = 10;
+  
+  //serial version
+  for (i=0; i<num_runs+1; i++) axpy(x, y, n, a);
+  
+  
+  /* cuda version */
+  
+  //warming-up
+  axpy_cuda(x_cuda, y_cuda, n, a);
+  
+  double elapsed = read_timer_ms();
+  for (i=0; i<num_runs; i++) axpy_cuda(x_cuda, y_cuda, n, a);
+  elapsed = (read_timer_ms() - elapsed)/num_runs;
+  
+  //using unified memory
+  //warming up
+    axpy_cudakernel_part1<<<(n+255)/256, 256>>>(x2, y2, n, a);
+    cudaDeviceSynchronize();
+
+    axpy_cpu_part2(x2, y2, n, a);
+
+    axpy_cudakernel_part3<<<(n+255)/256, 256>>>(x2, y2, n, a);
+    cudaDeviceSynchronize();
+
+    axpy_cpu_part4(x2, y2, n, a);
+    cudaDeviceSynchronize();
+
+    
+  double elapsed3 = read_timer_ms();
+  for (i=0; i<num_runs; i++){
+    axpy_cudakernel_part1<<<(n+255)/256, 256>>>(x2, y2, n, a);
+    cudaDeviceSynchronize();
+
+    axpy_cpu_part2(x2, y2, n, a);
+
+    axpy_cudakernel_part3<<<(n+255)/256, 256>>>(x2, y2, n, a);
+    cudaDeviceSynchronize();
+
+    axpy_cpu_part4(x2, y2, n, a);
+  }
+  elapsed3 = (read_timer_ms() - elapsed3)/(num_runs);
+  
+  //warming-up
+  axpy_cuda_optimized(x_cuda1, y_cuda1, n, a);
+  
+  double elapsed1 = read_timer_ms();
+  for (i=0; i<num_runs; i++) axpy_cuda_optimized(x_cuda1, y_cuda1, n, a);
+  elapsed1 = (read_timer_ms() - elapsed1)/num_runs;
+
+  REAL checkresulty = check(y_cuda, y, n);
+  REAL checkresultx = check(x_cuda, x, n);
+  REAL checkresulty1 = check(y_cuda1, y, n);
+  REAL checkresultx1 = check(x_cuda1, x, n);
+  REAL checkresulty2 = check(y2, y, n);
+  REAL checkresultx2 = check(x2, x, n);
+  printf("axpy(%d): check_x: %g, check_y: %g, time: %0.2fms\n", n, checkresultx, checkresulty, elapsed);
+  printf("axpy_unified(%d): check_x: %g, check_y: %g, time: %0.2fms\n", n, checkresultx2, checkresulty2, (elapsed2+elapsed3));
+  printf("axpy_optimized(%d): check_x: %g, check_y: %g, time: %0.2fms\n", n, checkresultx1, checkresulty1, elapsed1);
+  //assert (checkresult < 1.0e-10);
+
+  free(y_cuda);
+  free(x_cuda);
+  free(y_cuda1);
+  free(x_cuda1);
+ 
+  free(y);
+  free(x);
+  cudaFree(x2);
+  cudaFree(y2);
+  return 0;
+}
