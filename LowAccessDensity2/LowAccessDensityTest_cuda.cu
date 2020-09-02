2505f8cf6139d4d6802ad8de5ddc640933cef9e0
Yonghong Yan <yanyh15@gmail.com>
2020-09-02 10:08:40 -0400

    Update and rename LowAccessDensityTest_cuda_fixArraySize.cu to LowAccessDensityTest_cuda.cu

--- LowAccessDensity2/LowAccessDensityTest_cuda.cu
+++ LowAccessDensity2/LowAccessDensityTest_cuda.cu
@@ -46,7 +46,13 @@ LowAccessDensityTest_cudakernel(REAL* x, REAL* y, int n, REAL a, int stride)
     if (i < (n/stride)) y[i] = a*x[i*stride];
 }
 
-void LowAccessDensityTest_cuda(REAL* x, REAL* y, long int n, REAL a, int stride) {
+//We need to test discrete memory and unified memory, and also for fixed array size (vary stride) 
+//and for fix amount of memory access (vary stride)
+//So there are total 4 tests. They can use the same CUDA kernel, but the driver functions might need to 
+//be separate so it is easier to read.
+
+
+void LowAccessDensityTest_cuda_discrete_memory(REAL* x, REAL* y, long int n, REAL a, int stride) {
   REAL *d_x, *d_y;
   cudaMalloc(&d_x, n*sizeof(REAL));
   cudaMalloc(&d_y, (n/stride)*sizeof(REAL));
