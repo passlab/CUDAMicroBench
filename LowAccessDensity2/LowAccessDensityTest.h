b074175261361e70143a757660a0f67e741ba4f2
Xinyao Yi <yxyict@gmail.com>
2020-09-06 08:58:07 +0000

    Deleted useless code

--- LowAccessDensity2/LowAccessDensityTest.h
+++
@@ -1,9 +0,0 @@
-#define REAL float
-#ifdef __cplusplus
-extern "C" {
-#endif
-extern void LowAccessDensityTest_cuda(REAL* x, REAL* y, long int n, REAL a, int stride);
-extern void LowAccessDensityTest_cuda_unified(REAL* x, REAL* y, long int n, REAL a, int stride);
-#ifdef __cplusplus
-}
-#endif
