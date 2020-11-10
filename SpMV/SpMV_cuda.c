11927016fad846517df14c755aba2a19b794ab19
xinyao <yxyict@gmail.com>
2020-11-10 04:23:04 -0500

    Added testall.sh file

--- SpMV/SpMV_cuda.c
+++ SpMV/SpMV_cuda.c
@@ -15,8 +15,7 @@ double read_timer_ms() {
     return (double) tm.time * 1000.0 + (double) tm.millitm;
 }
 
-int num_rows = 1000;
-int nnz = 200;
+
 
 void zero(REAL *A, int n)
 {
@@ -24,19 +23,6 @@ void zero(REAL *A, int n)
     for (i = 0; i < n; i++) {
         A[i] = 0.0;
     }
-}
-
-void init_data(REAL *data, int *indices, REAL *matrix)
-{
-  int tmp = 0;
-	for (int i = 0; i < num_rows; i++) {
-	  for (int j = 0; j < num_rows; j++) {
-      if(matrix[i*num_rows+j] != 0) {
-        data[tmp] = matrix[i*num_rows+j];
-        indices[tmp] = j;
-        tmp++;}
-    }
-  }
 }
 
 /* initialize a vector with random floating point numbers */
@@ -49,10 +35,12 @@ void init_vector(REAL *vector, int m)
 
 
 
-void init_matrix(REAL * matrix) {
-    REAL d[num_rows*num_rows];
+void init_matrix(REAL * matrix, int num_rows,int nnz) {
+
+    REAL *d, *A;
+    d = (REAL *) malloc((num_rows * num_rows) * sizeof(REAL));
+    A = (REAL *) malloc((num_rows * num_rows) * sizeof(REAL));
     int i,j,n,a,b,t;
-    REAL A[num_rows][num_rows];
     srand(time(NULL));
     n=num_rows*num_rows;
     for (i=0;i<n;i++) d[i]=i;
@@ -63,13 +51,13 @@ void init_matrix(REAL * matrix) {
  
     for (i=0;i<num_rows;i++) {
         for (j=0;j<num_rows;j++) {
-            A[i][j]=(d[i*num_rows+j]>=nnz)?0:(REAL)(drand48()+1);
-            matrix[i*num_rows+j] = A[i][j];
+            A[i*num_rows+j]=(d[i*num_rows+j]>=nnz)?0:(REAL)(drand48()+1);
+            matrix[i*num_rows+j] = A[i*num_rows+j];
         }
     }
 }
 
-void init_ptr(int *ptr, REAL * matrix)
+void init_csr(int *ptr, REAL *data, int *indices, REAL *matrix, int num_rows, int nnz)
 {
   int tmp = 0;
   ptr[num_rows] = nnz;
@@ -77,16 +65,52 @@ void init_ptr(int *ptr, REAL * matrix)
   float * non_zero_elements;
   non_zero_elements = (float *) malloc(num_rows * sizeof(float));
 	for (int i = 0; i < num_rows; i++) {
-    int tmp = 0;
+    int tmp1 = 0;
 	  for (int j = 0; j < num_rows; j++) {
       if(matrix[i*num_rows+j] != 0) {
+        data[tmp] = matrix[i*num_rows+j];
+        indices[tmp] = j;
         tmp++;
+        tmp1++;
       }
-    non_zero_elements[i] = tmp;
+	  non_zero_elements[i] = tmp1;
     }
   }
   for (int i = 1; i<num_rows; i++){
     ptr[i] = ptr[i-1]+ non_zero_elements[i-1];
+  }
+}
+
+void init_index_count(int * row_nnz_start, int * row, int * column, REAL *matrix, int num_rows)
+{
+  int tmp = 0;
+  int count = 0;
+  row_nnz_start[0] = 0;
+	for (int i = 0; i < num_rows; i++) {
+    count = 0;
+	  for (int j = 0; j < num_rows; j++) {
+      if(matrix[i*num_rows+j] != 0) {
+        count++;
+        row[tmp] = i;
+        column[tmp] = j;
+        tmp++;
+      }
+    }
+    if( i < num_rows-1 ) row_nnz_start[i+1] = row_nnz_start[i] + count;
+  }
+}
+
+void init_index(int * row, int * column, REAL *matrix, int num_rows)
+{
+  int tmp = 0;
+	for (int i = 0; i < num_rows; i++) {
+	  for (int j = 0; j < num_rows; j++) {
+      if(matrix[i*num_rows+j] != 0) {
+        row[tmp] = i;
+        column[tmp] = j;
+        tmp++;
+      }
+    }
   }
 }
 
@@ -117,41 +141,84 @@ REAL check(REAL*A, REAL*B, int n)
 int main(int argc, char *argv[])
 {
   int *ptr, * indices;
-  float * data, * x, *y, *y_cuda, *y_normal;
+  float * data, * x, *y, *y_csr, *y_dense, *y_warmingup, *y_unified, *y_unified_count;
   float *matrix;
+  int num_rows = 0;
+  int nnz = 0;
+  //fprintf(stderr, "Usage: SpMV <n>\n");
+  if (argc >= 2) {
+    nnz = atoi(argv[1]);
+  }
+  if (argc >= 3) {
+    num_rows = atoi(argv[2]);
+  }
   
   ptr = (int *) malloc((num_rows+1) * sizeof(int));
   indices = (int *) malloc(nnz * sizeof(int));
   data = (float *) malloc(nnz * sizeof(float));
   x = (float *) malloc(num_rows * sizeof(float));
   y = (float *) malloc(num_rows * sizeof(float));
-  y_cuda = (float *) malloc(num_rows * sizeof(float));
-  y_normal = (float *) malloc(num_rows * sizeof(float));
+  y_csr = (float *) malloc(num_rows * sizeof(float));
+  y_dense = (float *) malloc(num_rows * sizeof(float));
+  y_unified = (float *) malloc(num_rows * sizeof(float));
+  y_unified_count = (float *) malloc(num_rows * sizeof(float));
+  y_warmingup = (float *) malloc(num_rows * sizeof(float));
   matrix = (float *) malloc(num_rows * num_rows * sizeof(float));
-  
+
   srand48(1<<12);
-  init_matrix(matrix);
+  init_matrix(matrix,num_rows,nnz);
+
   init_vector(x, num_rows);
-  init_data(data,indices, matrix);
-  init_ptr(ptr, matrix);
+  init_csr(ptr, data,indices, matrix, num_rows,nnz);
+  //init_ptr(ptr, matrix,num_rows,nnz);
 
   int i;
   int num_runs = 5;
   /* cuda version */
-  double elapsed = read_timer_ms();
-  for (i=0; i<num_runs; i++) spmv_csr_serial( num_rows, ptr, indices, data, x, y);
-  for (i=0; i<num_runs; i++) spmv_cuda( num_rows, ptr, indices, data, x, y_cuda, nnz, matrix, y_normal);
-  elapsed = (read_timer_ms() - elapsed)/num_runs;
+  //double elapsed = read_timer_ms();
+  double elapsed = 0;
+  double elapsed1 = 0;
+  double elapsed2 = 0;
+  double elapsed3 = 0;
+  double elapsed4 = 0;
 
-  printf("check:%f\n",check(y,y_cuda,num_rows));
-  printf("check1:%f\n",check(y,y_normal,num_rows));
-
+  spmv_csr_serial( num_rows, ptr, indices, data, x, y);
+  
+  //warmingup for dense_discrete
+  elapsed = spmv_cuda_dense_discrete( num_rows, x, nnz, matrix, y_warmingup);
+  //1) pass the full matrix via discrete memory, and regular MV
+  for (i=0; i<num_runs; i++) elapsed1 += spmv_cuda_dense_discrete( num_rows, x, nnz, matrix, y_dense);
+	
+  //warmingup for csr_discrete
+  elapsed = spmv_cuda_csr_discrete( num_rows, x, nnz, matrix, y_warmingup);
+  //2) pass the csr format of the matrix via discrete memery, and sparseMV	
+  for (i=0; i<num_runs; i++) elapsed2 += spmv_cuda_csr_discrete( num_rows, x, nnz, matrix, y_csr);
+  
+  //warmingup for unified memory
+  elapsed = spmv_cuda_unified( num_rows, x, nnz, matrix, y_warmingup);
+  //3) full matrix, pass indexes of non-zero elements, unified memory
+  for (i=0; i<num_runs; i++) elapsed3 += spmv_cuda_unified( num_rows, x, nnz, matrix, y_unified);
+  
+  //warmingup for unified_count
+  elapsed = spmv_cuda_unified_count( num_rows, x, nnz, matrix, y_warmingup);
+  //4) full matrix on unified memory, pass the index of non-zero elements, but not the element themselves.  	
+  for (i=0; i<num_runs; i++) elapsed4 += spmv_cuda_unified_count( num_rows, x, nnz, matrix, y_unified_count);
+  //elapsed = (read_timer_ms() - elapsed)/num_runs;
+  
+  printf("%d,%f,%f,%f,%f\n", nnz, elapsed1/num_runs, elapsed2/num_runs, elapsed3/num_runs, elapsed4/num_runs);
+  //printf("Spmv (csr) (%d): time: %0.2fms\n", nnz, elapsed2/num_runs);
+  //printf("Spmv (unified) (%d): time: %0.2fms\n", nnz, elapsed3/num_runs);
+  //printf("Spmv (unified_count) (%d): time: %0.2fms\n", nnz, elapsed4/num_runs);
+  //printf("check_dense:%f\n",check(y,y_csr,num_rows));
+  //printf("check_csr:%f\n",check(y,y_dense,num_rows));
+  //printf("check_unified:%f\n",check(y,y_unified,num_rows));
+  //printf("check_unified_count:%f\n",check(y,y_unified_count,num_rows));
   free(ptr);
   free(indices);
   free(data);
   free(x);
   free(y);
-  free(y_cuda);
-  free(y_normal);
+  free(y_csr);
+  free(y_dense);
   return 0;
 }
--- SpMV/SpMV_cudakernel.cu
+++ SpMV/SpMV_cudakernel.cu
@@ -1,6 +1,9 @@
 #include "SpMV.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-__global__ void spmv_csr_kernel(const int num_rows, const int *ptr, const int * indices, const REAL *data, const REAL * x, REAL *y)
+//2 csr-format of the matrix, copy csr formatted-matrix via discrete memory
+__global__ void spmv_csr(const int num_rows, const int *ptr, const int * indices, const REAL *data, const REAL * x, REAL *y)
 {
     int row = threadIdx.x + blockIdx.x * blockDim.x;
     if (row < num_rows){
@@ -15,7 +18,8 @@ __global__ void spmv_csr_kernel(const int num_rows, const int *ptr, const int * 
     }
 }
 
-__global__ void spmv_cudakernel_1perThread(REAL* matrix, REAL* vector, REAL *y, int num_rows)
+// 1) full matrix, discrete memory to copy the full matrix
+__global__ void spmv_dense(REAL* matrix, REAL* vector, REAL *y, int num_rows)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < num_rows) {
@@ -27,7 +31,7 @@ __global__ void spmv_cudakernel_1perThread(REAL* matrix, REAL* vector, REAL *y, 
     }
 }
 
-__global__ void spmv_cudakernel_1perThread_check_and_compute(REAL* matrix, REAL* vector, REAL *y, int num_rows)
+__global__ void spmv_dense_check_and_compute(REAL* matrix, REAL* vector, REAL *y, int num_rows)
 {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < num_rows) {
@@ -41,45 +45,198 @@ __global__ void spmv_cudakernel_1perThread_check_and_compute(REAL* matrix, REAL*
     }
 }
 
+// 3) kernels for full matrix stored in unified memory, data copied to GPU explicitly via cudaMemcpy are indexes of non-zero elements
+__global__ void spmv_unified(REAL* matrix_unified, const int num_rows, const int *rowNum, const int * columnNum, const REAL * x, REAL *y, int nnz)
+{
+    int row = threadIdx.x + blockIdx.x * blockDim.x;
+    REAL dot = 0;
+    for (int n = 0; n < nnz; n++){
+        if(rowNum[n] == row){
+           dot += matrix_unified[row * num_rows + columnNum[n]]* x[columnNum[n]];
+        }
+    }
+    y[row] = dot;
+}
 
-void spmv_cuda(const int num_rows, const int *ptr, const int * indices, const REAL *data, const REAL * x, REAL *y, int nnz, REAL* matrix, REAL *y_normal) {
+// 4) kernels for full matrix stored in unified memory, data copied to GPU explicitly via cudaMemcpy are indexes of non-zero elements, 
+// and the column of the first non-zero element of each row
+__global__ void spmv_unified_count(int * count, REAL* matrix_unified, const int num_rows, const int *rowNum, const int * columnNum, const REAL * x, REAL *y, int nnz)
+{
+    int row = threadIdx.x + blockIdx.x * blockDim.x;
+    int row_start = count[row];
+    int row_end = count[row+1];
+
+    REAL dot = 0;
+    for (int n = row_start; n < row_end; n++){
+      dot += matrix_unified[row * num_rows + columnNum[n]]* x[columnNum[n]];
+    }
+    y[row] = dot;
+}
+
+double spmv_cuda_dense_discrete(const int num_rows, const REAL * x, int nnz, REAL* matrix, REAL *y) {
+  double elapsed1 = read_timer_ms();
+
+  REAL * d_x, *d_matrix, *d_y;
+  cudaMalloc(&d_x, num_rows*sizeof(REAL));
+  cudaMalloc(&d_matrix, num_rows * num_rows * sizeof(REAL));
+  cudaMalloc(&d_y, num_rows*sizeof(REAL));
+
+  //timer start for H2D
+  cudaMemcpy(d_x, x, num_rows*sizeof(REAL), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_matrix, matrix, num_rows*num_rows*sizeof(REAL), cudaMemcpyHostToDevice);
+  //timer end for H2D
+    
+  //timer start for kernel
+  spmv_dense_check_and_compute<<<256, 256>>>(d_matrix, d_x, d_y, num_rows);
+  cudaDeviceSynchronize();
+  //timer stop for kernel
+
+ //timer start for D2H
+  cudaMemcpy(y, d_y, num_rows*sizeof(REAL), cudaMemcpyDeviceToHost);
+    //timer stop for D2H
+
+  cudaFree(d_x);
+  cudaFree(d_y);
+  cudaFree(d_matrix);
+  elapsed1 = (read_timer_ms() - elapsed1);
+  return elapsed1;
+}
+
+double spmv_cuda_csr_discrete(const int num_rows, const REAL * x, int nnz, REAL* matrix, REAL *y) {
+
+  int *ptr, * indices;
+  float * data;
+  
+  ptr = (int *) malloc((num_rows+1) * sizeof(int));
+  indices = (int *) malloc(nnz * sizeof(int));
+  data = (float *) malloc(nnz * sizeof(float));
+  
+  init_csr(ptr, data,indices, matrix, num_rows,nnz);
+  double elapsed1 = read_timer_ms();
+
   int *d_ptr, * d_indices;
-  REAL * d_data, * d_x, *d_y, *d_matrix, *d_y_normal;
+  REAL * d_data, * d_x, *d_y;
 
   cudaMalloc(&d_ptr, (num_rows+1)*sizeof(int));
   cudaMalloc(&d_indices, nnz*sizeof(int));
 
   cudaMalloc(&d_data, nnz*sizeof(REAL));
   cudaMalloc(&d_x, num_rows*sizeof(REAL));
+
   cudaMalloc(&d_y, num_rows*sizeof(REAL));
-  cudaMalloc(&d_matrix, num_rows * num_rows * sizeof(REAL));
-
-  cudaMalloc(&d_y_normal, num_rows*sizeof(REAL));
-
-
 
   cudaMemcpy(d_ptr, ptr, (num_rows+1)*sizeof(int), cudaMemcpyHostToDevice);
   cudaMemcpy(d_indices, indices, nnz*sizeof(int), cudaMemcpyHostToDevice);
   cudaMemcpy(d_data, data, nnz*sizeof(REAL), cudaMemcpyHostToDevice);
   cudaMemcpy(d_x, x, num_rows*sizeof(REAL), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_matrix, matrix, num_rows*num_rows*sizeof(REAL), cudaMemcpyHostToDevice);
 
 
-  spmv_csr_kernel<<<256,256>>>(num_rows,d_ptr, d_indices, d_data, d_x, d_y);
-  cudaDeviceSynchronize();
-  spmv_cudakernel_1perThread<<<256, 256>>>(d_matrix, d_x, d_y_normal, num_rows);
-  cudaDeviceSynchronize();
-  spmv_cudakernel_1perThread_check_and_compute<<<256, 256>>>(d_matrix, d_x, d_y_normal, num_rows);
+  spmv_csr<<<256,256>>>(num_rows,d_ptr, d_indices, d_data, d_x, d_y);
   cudaDeviceSynchronize();
   cudaMemcpy(y, d_y, num_rows*sizeof(REAL), cudaMemcpyDeviceToHost);
-  cudaMemcpy(y_normal, d_y_normal, num_rows*sizeof(REAL), cudaMemcpyDeviceToHost);
+  free(data);
+  free(ptr);
+  free(indices);
 
   cudaFree(d_ptr);
   cudaFree(d_indices);
   cudaFree(d_data);
   cudaFree(d_x);
-
   cudaFree(d_y);
-  cudaFree(d_y_normal);
-  cudaFree(d_matrix);
+  elapsed1 = (read_timer_ms() - elapsed1);
+  return elapsed1;
+
 }
+
+
+double spmv_cuda_unified(const int num_rows, const REAL * x, int nnz, REAL* matrix, REAL *y) {
+  int *row, * column;
+  row = (int *) malloc(nnz * sizeof(int));
+  column= (int *) malloc(nnz * sizeof(int));
+
+  double elapsed1 = read_timer_ms();
+  REAL *matrix_unified;
+  cudaMallocManaged(&matrix_unified, num_rows*num_rows*sizeof(REAL));
+  memcpy(matrix_unified, matrix, num_rows*num_rows*sizeof(REAL));
+  REAL * d_x, *d_y;
+    
+  init_index(row , column, matrix, num_rows);
+  //double elapsed1 = read_timer_ms();
+
+  int *d_row, * d_column;
+
+  cudaMalloc(&d_row, nnz*sizeof(int));
+  cudaMalloc(&d_column, nnz*sizeof(int));
+  cudaMalloc(&d_x, num_rows*sizeof(REAL));
+  cudaMalloc(&d_y, num_rows*sizeof(REAL));
+
+  cudaMemcpy(d_row, row, nnz*sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_column, column, nnz*sizeof(int), cudaMemcpyHostToDevice);
+
+  cudaMemcpy(d_x, x, num_rows*sizeof(REAL), cudaMemcpyHostToDevice);
+  spmv_unified<<<256,256>>>(matrix_unified, num_rows,d_row, d_column, d_x, d_y, nnz);
+  cudaDeviceSynchronize();
+  cudaMemcpy(y, d_y, num_rows*sizeof(REAL), cudaMemcpyDeviceToHost);
+
+  free(row);
+  free(column);
+  cudaFree(d_row);
+  cudaFree(d_column);
+  cudaFree(d_x);
+  cudaFree(d_y);
+  cudaFree(matrix_unified);
+  elapsed1 = (read_timer_ms() - elapsed1);
+  return elapsed1;
+
+}
+
+double spmv_cuda_unified_count(const int num_rows, const REAL * x, int nnz, REAL* matrix, REAL *y) {
+  int *row, * column, *count;
+  row = (int *) malloc(nnz * sizeof(int));
+  column= (int *) malloc(nnz * sizeof(int));
+  count = (int *) malloc(num_rows * sizeof(int));
+
+
+  double elapsed1 = read_timer_ms();
+  REAL *matrix_unified;
+  cudaMallocManaged(&matrix_unified, num_rows*num_rows*sizeof(REAL));
+  memcpy(matrix_unified, matrix, num_rows*num_rows*sizeof(REAL));
+  REAL * d_x, *d_y;
+    
+  init_index_count(count, row , column, matrix, num_rows);
+  //double elapsed1 = read_timer_ms();
+
+  int *d_row, * d_column, *d_count;
+
+  cudaMalloc(&d_row, nnz*sizeof(int));
+  cudaMalloc(&d_column, nnz*sizeof(int));
+  cudaMalloc(&d_count, num_rows*sizeof(int));
+
+  cudaMalloc(&d_x, num_rows*sizeof(REAL));
+  cudaMalloc(&d_y, num_rows*sizeof(REAL));
+
+  cudaMemcpy(d_row, row, nnz*sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_column, column, nnz*sizeof(int), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_count, count, num_rows*sizeof(int), cudaMemcpyHostToDevice);
+
+
+  cudaMemcpy(d_x, x, num_rows*sizeof(REAL), cudaMemcpyHostToDevice);
+  spmv_unified<<<256,256>>>(matrix_unified, num_rows,d_row, d_column, d_x, d_y, nnz);
+  cudaDeviceSynchronize();
+  cudaMemcpy(y, d_y, num_rows*sizeof(REAL), cudaMemcpyDeviceToHost);
+
+  free(row);
+  free(column);
+  free(count);
+  cudaFree(d_row);
+  cudaFree(d_column);
+  cudaFree(count);
+  cudaFree(d_x);
+  cudaFree(d_y);
+  cudaFree(matrix_unified);
+  elapsed1 = (read_timer_ms() - elapsed1);
+  return elapsed1;
+
+}
+
+
